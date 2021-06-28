b5e42d5222874f392bdea058b66d138a05657d5c
BenAyedEmna <emnabenayed.ca@gmail.com>
2021-06-28 15:28:32 +0200

    remove duplicate

--- Aeroport/Position.cs
+++ Aeroport/Position.cs
@@ -4,13 +4,7 @@ using System.Text;
 
 namespace aereport
 {
-    enum PositionSuivie
-    {
-        avion_sur_tapis,
-        avion_en_cours_de_planner,
-        avion_dans_le_ciel,
-        avion_en_cours_atterissage
-    }
+    
     class Position
     {
         private PositionSuivie position_suivie;
